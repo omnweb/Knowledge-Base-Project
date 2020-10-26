7bf6845c1ea411f9b85e53f287dafe62f9662ccd
netomatiazi <netoomn@gmail.com>
2020-10-26 17:27:24 -0300

    Creating method to list users

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -5,8 +5,35 @@
 </template>
 
 <script>
+import { baseApiUrl } from "@/global"; // Constnte com a url do backend
+import axios from "axios"; //biblioteca que faz as requisições
+
 export default {
   name: "UserAdmin",
+  data: function () {
+    return {
+      //Modo altera o botão do formulário
+      // Se tiver save e possuir id o método chama o put
+      //Se tiver o save e sem id ele chama o post e
+      // Se p botão save tiver sumido e aparecer o botão de remover ele chama o delete
+      mode: "save", // vai trocar entre o módulo de exclusão e o modo save
+      user: {}, // Inicia vazio, mas recebe as operações a serem realizadas no banco
+      users: [], // Será renderizado uma tabela com os usuários
+    };
+  },
+  methods: {
+    //Carregar lista de usuários do backend
+    loadUsers() {
+      const url = `${baseApiUrl}/users`;
+      axios.get(url).then((res) => {
+        this.users = res.data;
+        console.log(this.users);
+      });
+    },
+  },
+  mounted() {
+    this.loadUsers();
+  },
 };
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,7 +15,7 @@ import router from './config/router' // Importando instância de router
 Vue.config.productionTip = false
 
 // TEMPORÁRIO
-require('axios').defaults.headers.common['Autorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6Ikpvw6NvIiwiZW1haWwiOiJqb2FvQGhvdG1haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE2MDM3MjQ4MDgsImV4cCI6MTYwMzk4NDAwOH0.EcaKVEOXD9ZyVgN-CdxrfDHpfC-GKupCIx48wPhMZFY'
+require('axios').defaults.headers.common['Autorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik5ldG8gTWF0aWF6aSIsImVtYWlsIjoibmV0b21hdGlhemlAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjAzNzQyNTM5LCJleHAiOjE2MDQwMDE3Mzl9.AB1OzJrRZ4OpJaiGoCUSnAT7Ig-c-AhG_EMc7MoZrcg'
 
 new Vue({
   store, // Compartilhando entre os componentes
