08861498e3d8e77c61b56d2207408f8dd3be7f69
netomatiazi <netoomn@gmail.com>
2020-10-29 12:02:16 -0300

    Creating the style of the articleItem component, which displays a preview of articles by category

--- frontend/src/components/article/ArticleItem.vue
+++ frontend/src/components/article/ArticleItem.vue
@@ -8,24 +8,24 @@
         <img
           v-if="article.imageUrl"
           :src="article.imageUrl"
-          alt="Article image"
           height="150"
           width="150"
+          alt="Article"
         />
         <img
           v-else
           src="@/assets/article.png"
-          width="150"
           height="150"
-          alt="Article default image"
+          width="150"
+          alt="Article"
         />
-        <div class="article-item-info">
-          <h2>{{ article.name }}</h2>
-          <p>{{ article.description }}</p>
-          <span class="article-item-author">
-            <strong>Autor: </strong> {{ article.author }}
-          </span>
-        </div>
+      </div>
+      <div class="article-item-info">
+        <h2>{{ article.name }}</h2>
+        <p>{{ article.description }}</p>
+        <span class="article-item-author">
+          <strong>Autor: </strong> {{ article.author }}
+        </span>
       </div>
     </router-link>
   </div>
@@ -39,4 +39,45 @@ export default {
 </script>
 
 <style>
+.article-item {
+  border-radius: 8px;
+  margin-bottom: 20px;
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+}
+
+.article-item a {
+  display: flex;
+  align-items: flex-start;
+  text-decoration: none;
+  color: #000;
+}
+
+.article-item-info h2 {
+  font-size: 1.7rem;
+}
+
+.article-item-image {
+  padding-right: 20px;
+  margin-right: 20px;
+  border-right: 1px solid #aaa;
+}
+
+.article-item-image img {
+  border-radius: 5px;
+}
+
+.article-item-info {
+  display: flex;
+  flex-direction: column;
+  align-self: stretch;
+}
+
+.article-item-info p {
+  flex: 1;
+  color: #555;
+  font-size: 1.1rem;
+}
 </style>
